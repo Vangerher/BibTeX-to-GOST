88d21e19dd59e47e8cecf94178f78d4ed5e4b7a0
Vanger <vangerher@gmail.com>
2025-03-28 17:47:12 +0500

    Первая версия

---
+++ main.py
@@ -0,0 +1,106 @@
+import io
+import json
+import math
+import struct
+import subprocess
+import sys
+import time
+import os
+from datetime import datetime
+from logging import exception
+
+from argon2 import PasswordHasher
+from cryptography.fernet import Fernet
+import crcmod as crc
+import numpy as np
+import pandas as pd
+import serial
+import serial.tools.list_ports
+from PySide6.QtCore import QTimer, Signal, QObject, QThread, Qt
+from PySide6.QtWidgets import QApplication, QMainWindow, QDialog, QWidget, QComboBox, \
+    QFileDialog, QCheckBox, QPushButton, QTabWidget, QToolBar, QTableWidgetItem, QMenu, QTableWidget, QAbstractItemView
+from PySide6.QtGui import QIcon, QFontMetrics, QPainter, QColor, QPen, QAction, QBrush
+
+from ui_main import Ui_MainWindow
+
+class Main(QMainWindow):
+
+    def __init__(self):
+        super(Main, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+        self.initConnect()
+        self.paramDict = {
+            'title':"",
+            'author':"",
+            'journal':"",
+            'volume':"",
+            'pages':"",
+            'year':"",
+            'publisher' :"",
+            'number': '',
+            'DOI': '',
+        }
+
+    def initConnect(self):
+        self.ui.CopyBtn.clicked.connect(self.convert)
+        self.ui.ConvertBtn.clicked.connect(self.convert)
+
+    def convert(self):
+
+        def getDict():
+            inputText = self.ui.InputText.toPlainText()
+            slicedText = inputText.split('\n')
+            for parameter in slicedText:
+                text = parameter.replace("{", "").replace("}", "")
+                line = text.split('=')
+                line[0] = line[0].replace(' ','')
+
+                if len(line) > 1:
+                    line[1] = line[1][:-1] if line[1][-1] == ',' else line[1]
+
+                if line[0] in self.paramDict.keys():
+                    self.paramDict[line[0]] = line[1]
+
+        def processNames():
+            authorsPre = self.paramDict['author'].split(' and ')
+            authors = []
+            for author in authorsPre:
+                splittted = author.split(', ')
+                surname = splittted[0].replace('\\','').replace("'",'')
+                name = ''
+                for names in splittted[1].split(' '):
+                    name += names[0]+'. '
+                authors.append([name, surname])
+
+            print(authors)
+            if len(authors) > 4:
+                authors = authors[:4]
+                authors[3] = '[et. al.]'
+
+            self.paramDict['author'] = authors
+            print(authors)
+
+        getDict()
+        processNames()
+
+        if self.paramDict['number'] != '':
+            outText = (f'{self.paramDict['author'][0][1]}, {self.paramDict['author'][0][0]}{self.paramDict['title']}. / {', '.join(map(lambda x:''.join(x),self.paramDict['author']))} // {self.paramDict['publisher']}. '
+                       f'- {self.paramDict['year']}. - Vol. {self.paramDict['volume']}. - № {self.paramDict['number']}. - P. {self.paramDict['pages'].replace("--","-")}.')
+        else:
+            outText = (
+                f'{self.paramDict['author'][0][1]}, {self.paramDict['author'][0][0]} {self.paramDict['title']}. / {', '.join(map(lambda x:''.join(x),self.paramDict['author']))} // {self.paramDict['publisher']}. '
+                f'- {self.paramDict['year']}. - Vol. {self.paramDict['volume']}. - P. {self.paramDict['pages'].replace("--", "-")}.')
+
+        if self.paramDict['DOI'] != '':
+            outText += f' DOI: {self.paramDict['DOI']}.'
+        print(outText)
+        self.ui.OutputText.setPlainText(outText)
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+
+    window = Main()
+    window.show()
+
+    sys.exit(app.exec())
---
+++ ui_main.py
@@ -0,0 +1,84 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'untitledbAvNio.ui'
+##
+## Created by: Qt User Interface Compiler version 6.8.2
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide6.QtCore import (QCoreApplication, QDate, QDateTime, QLocale,
+    QMetaObject, QObject, QPoint, QRect,
+    QSize, QTime, QUrl, Qt)
+from PySide6.QtGui import (QBrush, QColor, QConicalGradient, QCursor,
+    QFont, QFontDatabase, QGradient, QIcon,
+    QImage, QKeySequence, QLinearGradient, QPainter,
+    QPalette, QPixmap, QRadialGradient, QTransform)
+from PySide6.QtWidgets import (QApplication, QHBoxLayout, QMainWindow, QMenuBar,
+    QPlainTextEdit, QPushButton, QSizePolicy, QSpacerItem,
+    QStatusBar, QVBoxLayout, QWidget)
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if not MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(804, 610)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.horizontalLayout = QHBoxLayout(self.centralwidget)
+        self.horizontalLayout.setObjectName(u"horizontalLayout")
+        self.InputText = QPlainTextEdit(self.centralwidget)
+        self.InputText.setObjectName(u"InputText")
+
+        self.horizontalLayout.addWidget(self.InputText)
+
+        self.verticalLayout = QVBoxLayout()
+        self.verticalLayout.setObjectName(u"verticalLayout")
+        self.verticalLayout.setContentsMargins(10, -1, 10, -1)
+        self.verticalSpacer = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer)
+
+        self.ConvertBtn = QPushButton(self.centralwidget)
+        self.ConvertBtn.setObjectName(u"ConvertBtn")
+
+        self.verticalLayout.addWidget(self.ConvertBtn)
+
+        self.CopyBtn = QPushButton(self.centralwidget)
+        self.CopyBtn.setObjectName(u"CopyBtn")
+
+        self.verticalLayout.addWidget(self.CopyBtn)
+
+        self.verticalSpacer_2 = QSpacerItem(20, 40, QSizePolicy.Policy.Minimum, QSizePolicy.Policy.Expanding)
+
+        self.verticalLayout.addItem(self.verticalSpacer_2)
+
+
+        self.horizontalLayout.addLayout(self.verticalLayout)
+
+        self.OutputText = QPlainTextEdit(self.centralwidget)
+        self.OutputText.setObjectName(u"OutputText")
+
+        self.horizontalLayout.addWidget(self.OutputText)
+
+        MainWindow.setCentralWidget(self.centralwidget)
+        self.menubar = QMenuBar(MainWindow)
+        self.menubar.setObjectName(u"menubar")
+        self.menubar.setGeometry(QRect(0, 0, 804, 22))
+        MainWindow.setMenuBar(self.menubar)
+        self.statusbar = QStatusBar(MainWindow)
+        self.statusbar.setObjectName(u"statusbar")
+        MainWindow.setStatusBar(self.statusbar)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"LaTeX-GOST converter", None))
+        self.ConvertBtn.setText(QCoreApplication.translate("MainWindow", u"->\u041f\u0440\u0435\u043e\u0431\u0440\u0430\u0437\u043e\u0432\u0430\u0442\u044c ->", None))
+        self.CopyBtn.setText(QCoreApplication.translate("MainWindow", u"\u041a\u043e\u043f\u0438\u0440\u043e\u0432\u0430\u0442\u044c->", None))
+    # retranslateUi
+
